81f0ce1c0ea4fc37d0d4ad82e70bb8d86787617e
Rohith <kantubhuktharohith@gmail.com>
2026-06-08 19:48:17 +0530

    feat: switch to HTML5 web history routing and add Vercel routing fallback config

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from './pages/Home.vue'
 import Skills from './pages/Skills.vue'
 import Hackathons from './pages/Hackathons.vue'
@@ -15,7 +15,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
